27c54e176cc78e23be0491d5aafe4556d0c4fddd
MacSiem <maciek.sieminski@gmail.com>
2026-03-18 10:34:17 +0100

    Fix flickering: increase throttle to 10s + add state hash check

--- ha-energy-optimizer.js
+++ ha-energy-optimizer.js
@@ -61,11 +61,14 @@
       this._lastRenderTime = now;
       return;
     }
-    if (now - (this._lastRenderTime || 0) < 5000) {
+    if (now - (this._lastRenderTime || 0) < 10000) {
       if (!this._renderScheduled) {
         this._renderScheduled = true;
         setTimeout(() => {
           this._renderScheduled = false;
+          const newHash = Object.keys(hass.states).length + '_' + (hass.states['sun.sun'] ? hass.states['sun.sun'].state : '');
+          if (newHash === this._lastStateHash) return;
+          this._lastStateHash = newHash;
       this._updateEnergyData();
           this._render();
           this._lastRenderTime = Date.now();
